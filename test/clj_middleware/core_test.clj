b19128e59f900ecff1f49fe03553bf2a2a3d315d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-12 00:22:19 +0100

    Project stub

---
+++ test/clj_middleware/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-middleware.core-test
+  (:require [clojure.test :refer :all]
+            [clj-middleware.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
