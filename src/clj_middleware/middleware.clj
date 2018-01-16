5f4a05a46b1c42671f7786a4d9f2367bcf155a1f
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-16 14:57:39 +0100

    Implementation of middleware

--- src/clj_middleware/middleware.clj
+++ src/clj_middleware/middleware.clj
@@ -1,6 +1,23 @@
-(ns clj-middleware.core)
+;
+;  (C) Copyright 2018  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns clj-middleware.middleware
+    "Definition(s) of middleware used with the cooperation with Clojure Ring.")
+
+(defn inject-configuration
+    "Inject configuration structure into the request parameter.
+     It means that the process request -> emender-waive -> response could be implemented
+     in a functional way."
+    [handler configuration]
+    (fn [request]
+        (handler (assoc request :configuration configuration))))
+
