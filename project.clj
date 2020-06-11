c50fc014108de4a6cc9951bc1d2fac72cc4be4ec
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-11 08:55:40 +0200

    Reformatted project.clj by zprint formatting tool

--- project.clj
+++ project.clj
@@ -11,13 +11,13 @@
 ;
 
 (defproject org.clojars.tisnik/clj-middleware "0.1.0-SNAPSHOT"
-    :description "Simple Ring middleware to inject configuration structure into the request parameter."
-    :url "https://github.com/tisnik/clj-middleware"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.8.0"]]
-    :plugins [[test2junit "1.1.0"]
-              [codox "0.8.11"]
-              [lein-cloverage "1.0.6"]]
-    :target-path "target/%s"
-    :profiles {:uberjar {:aot :all}})
+  :description "Simple Ring middleware to inject configuration structure into the request parameter."
+  :url "https://github.com/tisnik/clj-middleware"
+  :license {:name "Eclipse Public License",
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[test2junit "1.1.0"]
+            [codox "0.8.11"]
+            [lein-cloverage "1.0.6"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
