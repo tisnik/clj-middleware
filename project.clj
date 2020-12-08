cf085b471ea2a0ca287d0b564cf80b2468e0845e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-08 09:52:09 +0100

    Bump up dependency of lein-cloverage

--- project.clj
+++ project.clj
@@ -18,6 +18,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :plugins [[lein-codox "0.10.7"]
             [test2junit "1.1.0"]
-            [lein-cloverage "1.0.6"]]
+            ;[lein-test-out "0.3.1"]
+            [lein-cloverage "1.0.7-SNAPSHOT"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
