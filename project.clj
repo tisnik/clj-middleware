5ea401a6f2dd15ae0f884a96a2c5b04e4b84d7f2
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-29 08:45:33 +0100

    New dependency - project-edn

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2018, 2020  Pavel Tisnovsky
+;  (C) Copyright 2018, 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -22,6 +22,8 @@
             [lein-cloverage "1.0.7-SNAPSHOT"]
             [lein-kibit "0.1.8"]
             [lein-clean-m2 "0.1.2"]
+            [lein-project-edn "0.3.0"]
             [lein-marginalia "0.9.1"]]
+  :project-edn {:output-file "doc/details.clj"}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
